b33ebd0037456f4dbaabf03dabf6e3cf8591fb5d
Divyam Prusty <divyamprusty536@gmail.com>
2021-05-28 14:21:01 +0530

    Directory

--- myapp/index.js
+++ myapp/index.js
@@ -2,11 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.writeHead(301, {Location: `http://localhost:${port}/number`})
-    res.end()
-})
-
 app.get('/number', (req, res) => {
     res.send(`Put any number after http://localhost:${port}/number/ in the url to check whether it is ODD or EVEN`)
 })
